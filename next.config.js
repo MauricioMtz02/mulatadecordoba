217694e50e488c5b506fb383274aead8057dae38
Mauricio Martinez Martinez <mtz0mau2002@gmail.com>
2022-10-03 21:51:43 -0500

    deploy static v7

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.NODE_ENV !== 'production' ? '127.0.0.1' : 'api-mulata.mauricio-martinez.ga', 'ui-avatars.com'],
+    domains: [process.env.NODE_ENV !== 'production' ? '127.0.0.1' : 'mulata-blog.nesari.ga', 'ui-avatars.com'],
   },
   env: {
     URL_AVATAR: 'https://ui-avatars.com/api/?bold=true&background=F38630&color=fff',
-    BACKEND_IMAGES: process.env.NODE_ENV !== 'production' ? 'http://127.0.0.1:8000/img' : 'https://api-mulata.mauricio-martinez.ga/img',
-    URL_BACKEND: process.env.NODE_ENV !== 'production' ? 'http://127.0.0.1:8000/api' : 'https://api-mulata.mauricio-martinez.ga/api'
+    BACKEND_IMAGES: process.env.NODE_ENV !== 'production' ? 'http://127.0.0.1:8000/img' : 'https://mulata-blog.nesari.ga/img',
+    URL_BACKEND: process.env.NODE_ENV !== 'production' ? 'http://127.0.0.1:8000/api' : 'https://mulata-blog.nesari.ga/api'
   }
 }
 
