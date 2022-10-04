941144966d807be4db44009f4b72525223715e40
Mauricio Martinez Martinez <mtz0mau2002@gmail.com>
2022-10-03 21:58:33 -0500

    deploy static v8

--- pages/blog/[url].jsx
+++ pages/blog/[url].jsx
@@ -81,7 +81,7 @@ export async function getStaticPaths() {
 
     return {
         paths,
-        fallback: false, // can also be true or 'blocking'
+        fallback: true, // can also be true or 'blocking'
     }
 }
   
